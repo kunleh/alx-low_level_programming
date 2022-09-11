650116c4768837cc28efc059a62a1b5bd402251a
Oyekunle Ogunsina <oyekunleogunsina@gmail.com>
2022-09-11 13:24:40 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
-#include <string.h>
-int main()
+int main(void)
 {
-	strcpy(str1, "Programming is like building a multilingual puzzle");
-	puts(str1);
+	puts("\"Programming is like building a multilingual puzzle");
 	return(0);
 }
