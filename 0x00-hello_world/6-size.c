b5f2a8d90572ab5daf07e7c523d735a7263a531e
Oyekunle Ogunsina <oyekunleogunsina@gmail.com>
2022-09-11 14:16:12 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints sizeof different data
+ *
+ * Return: 0
+ *
+*/
+
+
+int main(void)
+{
+	printf("%lu\n", sizeof(char));
+	printf("%lu\n", sizeof(int));
+	printf("%lu\n", sizeof(float));
+	printf("%lu", sizeof(double));
+	reeturn (0);
+}
