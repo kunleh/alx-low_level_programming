0715e7ee2d141d63800fbba433ecc4e3b302f836
Oyekunle Ogunsina <oyekunleogunsina@gmail.com>
2022-09-13 00:06:32 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - print lowercase alphabet except q and e
+ *
+ * Return: 0
+ *
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'a';
+
+	while (c < 123 && c != 'q' && c != 'e')
+	{
+		putchar(c);
+		c++;
+	}
+	
+	putchar('\n');
+
+	return (0);
+}
