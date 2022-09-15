d53aecf5d3750c1f0377d078b429b07c4172c48e
Oyekunle Ogunsina <oyekunleogunsina@gmail.com>
2022-09-15 10:25:49 -0700

    2-alphabet

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print possible combinaiton of two digit
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int c;
+
+	for (c = 48; c <= 58; c++)
+	{
+		for  ( 
+	}
+
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_alphabet - print alphabet ten times
+ *
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+	char c;
+	
+	for (int i = 0;i <10;i++)
+	{
+	for (c = 'a';c <= 'z';,c++)
+	{
+		_putchar(c);
+	}
+
+	_putchar('\n');
+}
+}
