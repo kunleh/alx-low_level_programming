637e2ad73c878dacfff1693485564f6895c27dea
Oyekunle Ogunsina <oyekunleogunsina@gmail.com>
2022-09-14 21:11:04 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,22 @@
 #include <stdio.h>
 #include <main.h>
 /**
- * main - Print _putchar
+ * main - Print string  _putchar
  *
  * Return: 0
  */
 
 int main(void)
 {
-	printf("_puutchar\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+
 	return (0);
 
 }
