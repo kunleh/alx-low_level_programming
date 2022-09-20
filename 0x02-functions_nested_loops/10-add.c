56ed08cbb666d929b9291c54239afabba7bb6a5a
Oyekunle Ogunsina <oyekunleogunsina@gmail.com>
2022-09-20 04:59:52 -0700

    ./10-add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result
+ * @a: first number to be added
+ * @b: second number to added
+ *
+ * Description - add two integers and return the result
+ * Return: sum of addition
+ */
+
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+
+	return (sum);
+}
