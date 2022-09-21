d7300311874b7e5d261cc9cbb4f1e4d78d953d3f
Oyekunle Ogunsina <oyekunleogunsina@gmail.com>
2022-09-21 08:42:42 +0000

    104-fibonacci.c

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - prints first 98 fibonacci starting with 1 and 2
+ *
+ * Return: 0 on Success
+ */
+int main(void)
+{
+	int i;
+	unsigned long num1;
+	unsigned long num2;
+	unsigned long numx;
+	unsigned long sum;
+
+	num1 = 1;
+	num2 = 2;
+
+	for (i = 0; i <= 98; i++)
+	{
+		printf("%lu", num1);
+		numx = num1 + num2;
+		num1 = num2;
+		num2 = numx;
+	}
+		printf("\n");
+}
