00bb3b2694952234487053fc9d46e18c654306e0
Oyekunle Ogunsina <oyekunleogunsina@gmail.com>
2022-09-21 12:27:46 +0000

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,18 +1,48 @@
 #include <stdio.h>
 
 /**
- * main - print possible combinaiton of two digit
+ * main - print possible combinaiton of two two digit
  *
- * Return: 0
+ * Return: (0) for Success
  */
 
 int main(void)
 {
-	int c;
+	int i;
+	int j;
+	int k;
+	int l;
 
-	for (c = 48; c <= 58; c++)
+	for (i = 48; i < 58; i++)
 	{
-		for  ( 
+		for  (j = 48; j < 58; j++)
+		{
+			for (k = i; k < 58; k++)
+			{
+				if (k > i)
+					l = 48;
+				else
+					l = j + i;
+
+				for (; l < 58; l++)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(' ');
+					putchar(k);
+					putchar(l);
+
+					if (!(i == 57 && j == 56 && k == 57 && l == 57))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
 	}
+	putchar('\n');
+
+	return (0);
 
 }
