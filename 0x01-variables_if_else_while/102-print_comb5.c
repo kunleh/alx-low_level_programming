38e82304e63e81f2a2ada3ef5e00c15224f7ad4a
Oyekunle Ogunsina <oyekunleogunsina@gmail.com>
2022-09-21 13:08:21 +0000

    sth

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,48 +1,34 @@
 #include <stdio.h>
-
 /**
- * main - print possible combinaiton of two two digit
+ * main - Prints all possible combinations of a pair of
+ * two digit numbers, without any repetition.
  *
- * Return: (0) for Success
+ * Return: 0 on success.
  */
 
 int main(void)
 {
-	int i;
-	int j;
-	int k;
-	int l;
+	int i, j;
 
-	for (i = 48; i < 58; i++)
+	for (i = 0; i <= 99; i++)
 	{
-		for  (j = 48; j < 58; j++)
+		for (j = 0; j <= 99; j++)
 		{
-			for (k = i; k < 58; k++)
+			if (i < j && i != j)
 			{
-				if (k > i)
-					l = 48;
-				else
-					l = j + i;
-
-				for (; l < 58; l++)
+				putchar((i / 10) + '0');
+				putchar((i % 10) + '0');
+				putchar(' ');
+				putchar((j / 10) + '0');
+				putchar((j % 10) + '0');
+				if (i != 98 || j != 99)
 				{
-					putchar(i);
-					putchar(j);
+					putchar(',');
 					putchar(' ');
-					putchar(k);
-					putchar(l);
-
-					if (!(i == 57 && j == 56 && k == 57 && l == 57))
-					{
-						putchar(',');
-						putchar(' ');
-					}
 				}
 			}
 		}
 	}
 	putchar('\n');
-
 	return (0);
-
 }
